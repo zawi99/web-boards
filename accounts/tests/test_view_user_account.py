31de03edef2ace72863b4fda00c5617c083167e5
zawi99 <o2zawio2@gmail.com>
2019-01-04 23:16:01 +0100

    Added tests to UserAccountView

---
+++ accounts/tests/test_view_user_account.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+from ..views import UserAccountView
+
+
+class UserAccountTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('account-detail')
+
+
+class LoginRequiredUserAccountTests(UserAccountTestCase):
+    def test_redirection(self):
+        login_url = reverse('login')
+        response = self.client.get(self.url)
+        self.assertRedirects(response, f'{login_url}?next={self.url}')
+
+
+class UserAccountTests(UserAccountTestCase):
+    def setUp(self):
+        super().setUp()
+        username = 'john'
+        password = '123'
+        user = User.objects.create_user(username=username,
+                                        password=password)
+        self.client.login(username=username, password=password)
+        self.response = self.client.get(self.url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/account/')
+        self.assertEquals(view.func.view_class, UserAccountView)
+
+
